da7799fb67faba2d811fa5c9ac895faef6cc5c86
Prajwol <lamichhaneprajwool@gmail.com>
2026-01-22 10:17:09 +0545

    Implementing Simple chain

---
+++ Chains/simple_chain.py
@@ -0,0 +1,19 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser 
+
+load_dotenv()
+
+prompt=PromptTemplate(
+    template='Generate 5 unique business ideas about {topic}.',
+    input_variables=['topic'],
+
+)
+model = ChatOpenAI()
+parser=StrOutputParser()
+
+chain = prompt | model | parser
+
+result=chain.invoke({'topic':'Artificial Intelligence'})
+print(result)
